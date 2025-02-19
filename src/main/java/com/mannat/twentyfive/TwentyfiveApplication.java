8eafd91e2a1df357288ea13e780b67478bcf5a8f
HarmanatS <wish.mannat@gmail.com>
2025-02-19 18:25:19 +0530

    2025 - first commit

---
+++ src/main/java/com/mannat/twentyfive/TwentyfiveApplication.java
@@ -0,0 +1,13 @@
+package com.mannat.twentyfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwentyfiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwentyfiveApplication.class, args);
+	}
+
+}
